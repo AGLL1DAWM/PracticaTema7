ff92696f7308889fe08dd1f2fff2a87e92c19cb7
garcia20377@iesmarenostrum.com <garcia20377@iesmarenostrum.com>
2020-05-20 12:02:01 +0200

    Formulario Completo

--- PracricaTema7/PracricaTema7/Form1.Designer.cs
+++ PracricaTema7/PracricaTema7/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PracricaTema7
 {
-    partial class Form1
+    partial class PracticaTema7
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,20 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tNombre = new System.Windows.Forms.TextBox();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tTelefono = new System.Windows.Forms.TextBox();
+            this.lbApellidos = new System.Windows.Forms.Label();
+            this.tApellidos = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tListaSocios = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // tNombre
+            // 
+            this.tNombre.Location = new System.Drawing.Point(95, 38);
+            this.tNombre.Name = "tNombre";
+            this.tNombre.Size = new System.Drawing.Size(100, 20);
+            this.tNombre.TabIndex = 0;
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(29, 44);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(44, 13);
+            this.lbNombre.TabIndex = 1;
+            this.lbNombre.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(527, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Telefono";
+            // 
+            // tTelefono
+            // 
+            this.tTelefono.Location = new System.Drawing.Point(589, 41);
+            this.tTelefono.Name = "tTelefono";
+            this.tTelefono.Size = new System.Drawing.Size(145, 20);
+            this.tTelefono.TabIndex = 2;
+            // 
+            // lbApellidos
+            // 
+            this.lbApellidos.AutoSize = true;
+            this.lbApellidos.Location = new System.Drawing.Point(260, 44);
+            this.lbApellidos.Name = "lbApellidos";
+            this.lbApellidos.Size = new System.Drawing.Size(49, 13);
+            this.lbApellidos.TabIndex = 5;
+            this.lbApellidos.Text = "Apellidos";
+            // 
+            // tApellidos
+            // 
+            this.tApellidos.Location = new System.Drawing.Point(326, 38);
+            this.tApellidos.Name = "tApellidos";
+            this.tApellidos.Size = new System.Drawing.Size(136, 20);
+            this.tApellidos.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tListaSocios
+            // 
+            this.tListaSocios.Location = new System.Drawing.Point(60, 143);
+            this.tListaSocios.Multiline = true;
+            this.tListaSocios.Name = "tListaSocios";
+            this.tListaSocios.Size = new System.Drawing.Size(674, 192);
+            this.tListaSocios.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Lista Socios";
+            // 
+            // PracticaTema7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 263);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(792, 347);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tListaSocios);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbApellidos);
+            this.Controls.Add(this.tApellidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tTelefono);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.tNombre);
+            this.Name = "PracticaTema7";
             this.Text = "PracticaTema7";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tNombre;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tTelefono;
+        private System.Windows.Forms.Label lbApellidos;
+        private System.Windows.Forms.TextBox tApellidos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tListaSocios;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- PracricaTema7/PracricaTema7/Form1.cs
+++ PracricaTema7/PracricaTema7/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PracricaTema7
 {
-    public partial class Form1 : Form
+    public partial class PracticaTema7 : Form
     {
-        public Form1()
+        public PracticaTema7()
         {
             InitializeComponent();
         }
